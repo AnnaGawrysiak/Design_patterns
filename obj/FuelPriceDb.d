dc0335cada9cba4ef20c2d931e790425f0eae228
Anna Gawrysiak <70595292+AnnaGawrysiak@users.noreply.github.com>
2022-10-09 20:42:49 +0200

    Delete obj directory

--- obj/FuelPriceDb.d
+++
@@ -1,9 +0,0 @@
-obj/FuelPriceDb.o: src/FuelPriceDb.cpp src/FuelPriceDb.hpp \
- src/EmailSender.hpp src/DbClient.hpp \
- src/solution/SubscribersInterface.hpp src/Calculator.hpp src/Chart.hpp
-src/FuelPriceDb.hpp:
-src/EmailSender.hpp:
-src/DbClient.hpp:
-src/solution/SubscribersInterface.hpp:
-src/Calculator.hpp:
-src/Chart.hpp:
